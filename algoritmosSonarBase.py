541ecbceec9c747cf1d61b93d417fc15b9087425
aortizd <agaldior@gmail.com>
2020-08-08 00:32:19 -0300

    Primera fase: Comparación de algoritmos simples.

--- algoritmosSonarBase.py
+++ algoritmosSonarBase.py
@@ -1,7 +1,113 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 13 21:19:21 2020
+# User: aortizd email:agaldior@gmail.com
+import numpy as np
+from matplotlib import pyplot
+from pandas import read_csv, set_option
+#from pandas.tools.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, KFold, cross_val_score, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-@author: agald
+# load dataset
+dataset = read_csv('sonar.csv', header=None)
+
 """
+# shape
+results = dataset.shape
+print(results)
+
+# types
+set_option('display.max_rows', 500)
+results = dataset.dtypes
+print(results)
+
+# peek at data
+set_option('display.width', 100)
+results = dataset.head(20)
+print(results)
+
+# describe data
+set_option('precision', 3)
+results = dataset.describe()
+print(results)
+
+# class distribution
+results = dataset.groupby(60).size()
+print(results)
+
+# histograms
+dataset.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1, figsize=(12,12))
+pyplot.show()
+
+# density
+dataset.plot(kind='density', subplots=True, layout=(8,8), sharex=False, legend=False, fontsize=1, figsize=(12,12))
+pyplot.show()
+
+# correlation matrix
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+fig.set_size_inches(10,10)
+pyplot.show()
+"""
+
+# split out validation dataset for the end
+array = dataset.values
+X = array[:,0:-1].astype(float)
+Y = array[:,-1]
+validation_size = 0.2
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, shuffle=True, random_state=seed)
+
+# test options
+num_folds = 10
+seed = 7
+scoring = 'accuracy'
+
+"""
+# spot check some algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+"""
+
+# standardized the dataset
+pipelines = []
+pipelines.append(('ScaledLR', Pipeline([('Scaler', StandardScaler()), ('LR', LogisticRegression())])))
+pipelines.append(('ScaledLDA', Pipeline([('Scaler', StandardScaler()), ('LDA', LinearDiscriminantAnalysis())])))
+pipelines.append(('ScaledKNN', Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsClassifier())])))
+pipelines.append(('ScaledCART', Pipeline([('Scaler', StandardScaler()), ('CART', DecisionTreeClassifier())])))
+pipelines.append(('ScaledNB', Pipeline([('Scaler', StandardScaler()), ('NB', GaussianNB())])))
+pipelines.append(('ScaledSVM', Pipeline([('Scaler', StandardScaler()), ('SVM', SVC())])))
+
+results = []
+names = []
+for name, model in pipelines:
+    kfold = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+    
+# compare algorithms
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(names)
+fig.set_size_inches(8,6)
+pyplot.show()
 
